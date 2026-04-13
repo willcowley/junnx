cc21af477ee298357af22056dfd297746f145166
william cowley <william.cowley@machine-discovery.com>
2026-04-13 23:53:42 +0100

    add data transform

--- experiments/scripts/mnist_ood.py
+++ experiments/scripts/mnist_ood.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from omegaconf import DictConfig
 from utils import instantiate_assert_type
 
-from junnx.datasets import DataLoader, TensorDataset
+from junnx.datasets import DataLoader, DataTransformFn, TensorDataset, TransformedTensorDataset
 from junnx.likelihoods import Likelihood
 from junnx.metrics import EntropyAUROC
 from junnx.net import StochasticNet
@@ -20,13 +20,28 @@ from junnx.variational import VariationalDistribution
 def _main(cfg: DictConfig) -> None:
 
     ds = instantiate_assert_type(cfg.ds, TensorDataset)
-    val_ds = instantiate_assert_type(cfg.val_ds, TensorDataset)
-    # context_ds = instantiate_assert_type(cfg.context_ds, TensorDataset)
-    ood_ds = instantiate_assert_type(cfg.ood_ds, TensorDataset)
+    ds_x_transform = instantiate_assert_type(cfg.ds_x_transform, DataTransformFn)
+    x_transform = ds_x_transform.fit(ds.x)
+    ds = TransformedTensorDataset(ds, xbijector=x_transform)
 
-    detect_ood_ds = TensorDataset(
-        x=jnp.concatenate([ood_ds.x, val_ds.x], axis=0),
-        y=jnp.concatenate([jnp.ones_like(ood_ds.y), jnp.zeros_like(val_ds.y)], axis=0),
+    val_ds = TransformedTensorDataset(
+        ds=instantiate_assert_type(cfg.val_ds, TensorDataset), xbijector=x_transform
+    )
+    context_ds = TransformedTensorDataset(
+        instantiate_assert_type(cfg.context_ds, TensorDataset),
+        xbijector=x_transform,
+    )
+    ood_ds = TransformedTensorDataset(
+        instantiate_assert_type(cfg.ood_ds, TensorDataset),
+        xbijector=x_transform,
+    )
+
+    detect_ood_ds = TransformedTensorDataset(
+        ds=TensorDataset(
+            x=jnp.concatenate([ood_ds.x, val_ds.x], axis=0),
+            y=jnp.concatenate([jnp.ones_like(ood_ds.y), jnp.zeros_like(val_ds.y)], axis=0),
+        ),
+        xbijector=x_transform,
     )
 
     key_dl = instantiate_assert_type(cfg.key_dl, jnp.ndarray)
@@ -39,7 +54,7 @@ def _main(cfg: DictConfig) -> None:
     net = instantiate_assert_type(cfg.net, StochasticNet)
     likelihood = instantiate_assert_type(cfg.likelihood, Likelihood)
     prior = instantiate_assert_type(cfg.prior, Prior)
-    sampler = instantiate_assert_type(cfg.sampler, Sampler)
+    sampler = instantiate_assert_type(cfg.sampler, Sampler, data=context_ds)
     variational_dist = instantiate_assert_type(cfg.variational, VariationalDistribution)
 
     model = TrainingModel(
