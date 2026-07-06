f88039ef1c9365f3494138e7f46c7bdb8c756bb4
william cowley <william.cowley@machine-discovery.com>
2026-07-06 23:23:29 +0100

    nb updates

--- docs/examples/shm_node.py
+++ docs/examples/shm_node.py
@@ -35,7 +35,7 @@ from tensorboardX import SummaryWriter
 from junnx.datasets import DataLoader, TensorDataset
 from junnx.layers import DenseStochasticLayer
 from junnx.likelihoods import GaussianLikelihood
-from junnx.loss_fns import TractableFSVILoss
+from junnx.loss_fns import NLLLoss, TractableFSVILoss
 from junnx.model import TrainingModel
 from junnx.net import _ACTIVATIONS, StochasticNet, TractableStochasticNet
 from junnx.priors import RBFPrior
@@ -57,7 +57,7 @@ def shm_vector_field(t, y, args):
 
 y0 = jnp.array([1.0, 0.0])
 
-t_grid = jnp.linspace(0, jnp.pi, 101)
+t_grid = jnp.linspace(0, 2 * jnp.pi * 3 / 4, 401)
 sol = diffeqsolve(
     terms=ODETerm(shm_vector_field),
     solver=Tsit5(),
@@ -70,10 +70,6 @@ sol = diffeqsolve(
 )
 
 xs, xdots = sol.ys.T
-
-# %%
-fig, ax = plt.subplots(1, 1, figsize=(4, 4))
-ax.plot(t_grid, xs**2 + xdots**2)
 
 # %%
 
@@ -176,9 +172,13 @@ class StochasticHamiltonianNODE(StochasticNet):
     vector_field: HamiltonianVectorField
     y0: jnp.ndarray
 
-    def __init__(self, n_states: int, n_hidden: int, depth: int, *, key) -> None:
+    def __init__(
+        self, n_states: int, n_hidden: int, depth: int, use_bias: bool, *, key
+    ) -> None:
         self.vector_field = HamiltonianVectorField(
-            hamiltonian_net=TractableMLP(n_states, 1, n_hidden, depth, use_bias=True, key=key)
+            hamiltonian_net=TractableMLP(
+                n_states, 1, n_hidden, depth, use_bias=use_bias, key=key
+            )
         )
         self.y0 = jnp.array([1.0, 0.0])  # initial conditions
 
@@ -210,28 +210,27 @@ class StochasticHamiltonianNODE(StochasticNet):
 
 
 # %%
-
-
 ds = TensorDataset(
     x=t_grid[idxs][:N_data],
     y=jnp.stack([xs[idxs][:N_data], xdots[idxs][:N_data]], axis=-1) + noise,
 )
 
 # %%
-
+sqrt2o2 = jnp.sqrt(2.0).item() / 2.0
 model = TrainingModel(
-    net=StochasticHamiltonianNODE(n_states=2, n_hidden=64, depth=3, key=jr.PRNGKey(20260704)),
+    net=StochasticHamiltonianNODE(
+        n_states=2, n_hidden=128, depth=3, use_bias=True, key=jr.PRNGKey(20260704)
+    ),
     likelihood=GaussianLikelihood(
         scale_init=(1.0, 1.0),
         bijector=tfp.bijectors.Softplus(),
     ),
-    prior=RBFPrior(lengthscales=(1.0, 1.0)),
+    prior=RBFPrior(lengthscales=(sqrt2o2, sqrt2o2)),
     variational_dist=GaussianVariationalDistribution(),
 )
 
+
 # %%
-
-
 class TractableHamiltonianFSVILoss(TractableFSVILoss):
 
     def get_tractable_net(self, model: TrainingModel) -> TractableStochasticNet:
@@ -241,12 +240,13 @@ class TractableHamiltonianFSVILoss(TractableFSVILoss):
 
 
 # %%
+Z_MAX = 4.0
 
-
+# %%
 _N_SAMPLES = 8
 loss_fn_fsvi = TractableHamiltonianFSVILoss(n_samples_nll=_N_SAMPLES)
-
-sampler = UniformSampler(n_dim=2, n_samples=64, low=(-2.0, -2.0), high=(2.0, 2.0))
+loss_fn_nll = NLLLoss(n_samples_nll=_N_SAMPLES)
+sampler = UniformSampler(n_dim=2, n_samples=64, low=(-Z_MAX, -Z_MAX), high=(Z_MAX, Z_MAX))
 dl = DataLoader(ds, batch_size=32, shuffle=False, key=jr.PRNGKey(20260703))
 
 opt = optax.adam(1e-3)
@@ -259,6 +259,7 @@ trainer = Trainer(
     n_epochs=1_500,
     opt=opt,
     loss_fn=loss_fn_fsvi,
+    # loss_fn=loss_fn_nll,
     logger=logger,
 )
 trainer.train(model, dl, sampler, key=jr.PRNGKey(20260704))
@@ -268,29 +269,28 @@ m = trainer.best_model
 # plot hamiltonian estimates
 
 n_grid = 101
-x_grid = jnp.linspace(-2, 2, n_grid)
-xdot_grid = jnp.linspace(-2, 2, n_grid)
+x_grid = jnp.linspace(-Z_MAX / 2, Z_MAX / 2, n_grid)
+xdot_grid = jnp.linspace(-Z_MAX / 2, Z_MAX / 2, n_grid)
 x0, x1 = jnp.meshgrid(x_grid, xdot_grid)
 z = jnp.concat([x0.reshape(-1, 1), x1.reshape(-1, 1)], axis=-1)  # [N*N, 2]
 
 m_hamiltonian = m.net.vector_field.hamiltonian_net
 
 key_pred = jr.PRNGKey(20260705)
-hs = m_hamiltonian.predict_f_samples(z, 32, key=key_pred)
+hs = m_hamiltonian.predict_f_samples(z, 128, key=key_pred)
 
 
 # %%
-fig, axes = plt.subplots(1, 3, figsize=(12 + 1.5, 4))
-ax0, ax1, ax2 = axes
+fig, axes = plt.subplots(1, 2, figsize=(4 * 2 + 1.0, 4))
+ax0, ax1 = axes
 
 ground_truth = 0.5 * (z[:, :1] ** 2 + z[:, 1:] ** 2).reshape(n_grid, n_grid)
 mean = hs.mean(axis=0).reshape(n_grid, n_grid)
 var = hs.var(axis=0).reshape(n_grid, n_grid)
-imshow_kwargs = {"extent": [-2, 2, -2, 2], "origin": "lower"}
+imshow_kwargs = {"extent": [-4, 4, -4, 4], "origin": "lower"}
 ax0.imshow(ground_truth, **imshow_kwargs)
 ax1.imshow(mean, **imshow_kwargs)
-ax2.imshow(var, **imshow_kwargs, vmin=0.25, vmax=1.0)
-for _ax in (ax0, ax1, ax2):
+for _ax in (ax0, ax1):
     _ax.scatter(*ds.y.T, s=6, edgecolor="w", linewidths=0.5, color="#4E79A7")
 fig.savefig(f"{logdir}/shm_node_hamiltonian_{timestamp}.png", dpi=200)
 
@@ -298,35 +298,69 @@ fig.savefig(f"{logdir}/shm_node_hamiltonian_{timestamp}.png", dpi=200)
 # %%
 dummy_t = jnp.zeros(shape=())
 
-n_grid = 21
-x_grid = jnp.linspace(-2, 2, n_grid)
-xdot_grid = jnp.linspace(-2, 2, n_grid)
+n_gridq = 11
+x_grid = jnp.linspace(-Z_MAX / 2, Z_MAX / 2, n_gridq)
+xdot_grid = jnp.linspace(-Z_MAX / 2, Z_MAX / 2, n_gridq)
 x0, x1 = jnp.meshgrid(x_grid, xdot_grid)
-z = jnp.concat([x0.reshape(-1, 1), x1.reshape(-1, 1)], axis=-1)  # [N*N, 2]
+zq = jnp.concat([x0.reshape(-1, 1), x1.reshape(-1, 1)], axis=-1)  # [N*N, 2]
 
-d_ground_truth = shm_vector_field(dummy_t, (z[:, 0], z[:, 1]), (1.0,))
+d_ground_truth = shm_vector_field(dummy_t, (zq[:, 0], zq[:, 1]), (1.0,))
 
-ks = jr.split(jr.PRNGKey(0), 64)
-dz = jax.vmap(
-    jax.vmap(lambda _z, _k: m.net.vector_field(dummy_t, _z, args=(_k,)), in_axes=(0, None)),
-    in_axes=(None, 0),
-)(z, ks)
+ks = jr.split(jr.PRNGKey(0), 256)
+dz_fn = jax.jit(
+    jax.vmap(
+        jax.vmap(
+            lambda _z, _k: m.net.vector_field(dummy_t, _z, args=(_k,)), in_axes=(0, None)
+        ),
+        in_axes=(None, 0),
+    )
+)
+dz = dz_fn(zq, ks)
 
 # %%
-fig, axes = plt.subplots(1, 2, figsize=(4 * 2 + 0.5, 4))
-ax0, ax1 = axes
-ax0.quiver(*z.T, *d_ground_truth)
-ax1.quiver(*z.T, *jnp.mean(dz, axis=0).T)
-for _ax in (ax0, ax1):
+fig, axes = plt.subplots(1, 3, figsize=(4 * 3 + 0.5, 4))
+ax0, ax1, ax2 = axes
+quiver_kwargs = {"scale": 15, "scale_units": "inches"}
+ax0.quiver(*zq.T, *d_ground_truth, **quiver_kwargs)
+ax1.quiver(*zq.T, *jnp.mean(dz, axis=0).T, **quiver_kwargs)
+# ax2.quiver(*z.T, *jnp.var(dz, axis=0).T)
+dz_ = dz_fn(z, ks)
+dz_var = jnp.var(dz_, axis=0).sum(axis=-1)
+ax2.imshow(
+    dz_var.reshape(n_grid, n_grid),
+    extent=[-Z_MAX / 2, Z_MAX / 2, -Z_MAX / 2, Z_MAX / 2],
+    origin="lower",
+    vmin=0.0,
+    vmax=2.0,
+)
+# ax2.scatter(*z.T, c=dz_var, vmin=0.0, vmax=2.0)
+for _ax in (ax0, ax1, ax2):
     _ax.scatter(*ds.y.T, s=8, edgecolor="w", linewidths=0.5, color="#4E79A7")
 
 
 # %%
-n_pred_cycle = 4
+dz_var.max()
+
+# %%
+n_pred_cycle = 8
 t_pred = jnp.linspace(0, 2 * jnp.pi * n_pred_cycle, 100 * n_pred_cycle + 1)
-key_pred = jr.PRNGKey(1)
+key_pred = jr.PRNGKey(0)
 
 f_samples = m.net.predict_f_samples(t_pred, n_samples=8, key=key_pred)
+
+# %%
+m_new_y0 = eqx.tree_at(lambda _m: _m.net.y0, m, jnp.array([0.1, 0.1]))
+f_samples_new_y0 = m_new_y0.net.predict_f_samples(t_pred, n_samples=8, key=key_pred)
+
+# %%
+fig, axes = plt.subplots(3, 2, figsize=(4 * 2 + 0.5, 4 * 3 + 0.5))
+for ax, f in zip(axes.ravel(), f_samples_new_y0):
+    ax.scatter(ds.y[:, 0], ds.y[:, 1], s=4)
+    ax.plot(f[:, 0], f[:, 1], lw=0.5)
+    ax.set_xlim(-4, 4)
+    ax.set_ylim(-4, 4)
+
+fig.savefig(f"{logdir}/shm_node_results_{timestamp}.png", dpi=200)
 
 # %%
 
@@ -334,8 +368,8 @@ fig, axes = plt.subplots(3, 2, figsize=(4 * 2 + 0.5, 4 * 3 + 0.5))
 for ax, f in zip(axes.ravel(), f_samples):
     ax.scatter(ds.y[:, 0], ds.y[:, 1], s=4)
     ax.plot(f[:, 0], f[:, 1], lw=0.5)
-    ax.set_xlim(-2, 2)
-    ax.set_ylim(-2, 2)
+    ax.set_xlim(-4, 4)
+    ax.set_ylim(-4, 4)
 
 fig.savefig(f"{logdir}/shm_node_results_{timestamp}.png", dpi=200)
 
