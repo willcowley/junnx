28ca722ef099628b16f27674c817e5d2b673f091
william cowley <william.cowley@machine-discovery.com>
2026-07-08 19:19:08 +0100

    Deployed 2226f00 with MkDocs version: 1.6.1

---
+++ assets/mathjax.js
@@ -0,0 +1,20 @@
+window.MathJax = {
+  tex: {
+    tags: "ams",
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex", "mathjax-process"
+  }
+};
+
+document$.subscribe(() => {
+  MathJax.startup.output.clearCache()
+  MathJax.typesetClear()
+  MathJax.texReset()
+  MathJax.typesetPromise()
+})
